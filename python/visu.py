9fb5fd11283b81337564fa92f67223348d47669f
Noah <noah.cuau@etu.u-bordeaux.fr>
2025-06-26 10:04:39 +0200

    bug fix

--- python/visu.py
+++ python/visu.py
@@ -196,14 +196,22 @@ def make_list_of_subgraph(nt_graph, graph):
         sub_graph_obj_list[nb_graph] = Sub_graph_nt(nb_graph,get_nx_node(leave+str(nb_graph), nt_graph))
         find_path_to_root(find_node_from_data(graph, leave), nt_graph, nb_graph, sub_graph_obj_list[nb_graph])
         nb_graph +=1
+
+  
+
     nb_node_per_graph = []
     for i in sub_graph_obj_list.keys():
         nb_node_per_graph.append((sub_graph_obj_list[i].max_x, sub_graph_obj_list[i]))
     nb_node_per_graph = sorted(nb_node_per_graph, key = lambda a : a[0])
+    
 
     for i, graph in enumerate(nb_node_per_graph):
         pass
         graph[1].change_graph_start_pos((0, i*UNIT*2), nt_graph)
+    id = nt_graph.nodes.index(get_nx_node('imaginary0', nt_graph))
+    nt_graph.nodes.pop(id)
+
+
    
 
         
@@ -228,7 +236,9 @@ def find_path_to_root(node, nt_graph, nb_sub_graph,  sub_graph, depth = 0, done 
                 i +=1
                 sub_graph.add_node(get_nx_node(parent.data+str(nb_sub_graph), nt_graph))
         for parent in parents:
-            find_path_to_root(parent, nt_graph, nb_sub_graph,sub_graph, depth, done ) 
+            find_path_to_root(parent, nt_graph, nb_sub_graph,sub_graph, depth, done )
+        
+        
 
 
 
